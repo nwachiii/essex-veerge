db52144d06e728ba0d3a3c146e62330cb5409b51
odbruce <odera996@gmail.com>
2025-04-24 16:16:31 +0100

    transaction page

---
+++ src/components/transactionDemoPage/transactionDetailsCard.js
@@ -0,0 +1,60 @@
+import {Box, Flex, Stack, Text} from '@chakra-ui/react';
+import React from 'react';
+import {themeStyles} from 'theme';
+import DownloadCsv from 'ui-lib/ui-lib.components/Button/downloadCsv';
+import {FormatToColorfulAdaptiveCurrency, FormatToColorfulCurrency} from 'utils/formatAmount';
+
+const TransactionDetailsCard = () => {
+  const overview = [
+    {
+      name: 'Net Transaction',
+      value: '135.93',
+      color: themeStyles.color.primary,
+    },
+    {
+      name: 'Total Deposit',
+      value: '6,754.10',
+      color: themeStyles.color.matador__green,
+    },
+    {
+      name: 'Total Debit',
+      value: '1,465.72',
+      color: '#f04438',
+    },
+  ];
+  return (
+    <Flex
+      h="full"
+      mb="32px"
+      minH="140px"
+      alignItems="center"
+      bg="#ffffff"
+      p="32px 48px"
+      rounded="12px"
+      border="0.5px solid #e4e4e7"
+      w="full"
+      justifyContent="space-between"
+    >
+      {overview.map((item, idx) => (
+        <Stack key={idx} spacing="12px">
+          <Text fontSize="16px" fontWeight="500" color="#18181b">
+            {item.name}
+          </Text>
+
+          <FormatToColorfulAdaptiveCurrency
+            amount={item?.value}
+            lens={13}
+            color={item.color}
+            maxSize={32}
+            minSize={10}
+            pow={0.92}
+            fontWeight="600"
+            decimalStyle={{fontWeight: '600', color: '#606060'}}
+          />
+        </Stack>
+      ))}
+    </Flex>
+  );
+};
+
+export default TransactionDetailsCard;
---
+++ src/pages/residents/customer_overview/CustomersTable/Table/transactionCOLUMN.js
@@ -0,0 +1,104 @@
+import {themeStyles} from 'theme';
+import {FormatToColorfulAdaptiveCurrency} from 'utils/formatAmount';
+
+const {Button, Badge, Text} = require('@chakra-ui/react');
+const {changeDateFormat} = require('utils/formatDate');
+
+export const TRANSACTIONPAGE_COLUMN = [
+  {
+    Header: 'Date',
+    accessor: row => {
+      return (
+        <Text fontSize={'16px'} fontWeight="400" color="#191919">
+          {row.date}
+        </Text>
+      );
+    },
+  },
+  {
+    Header: 'Description',
+    accessor: row => {
+      return (
+        <Text fontSize={'16px'} fontWeight="400" color="#191919">
+          {row.description}
+        </Text>
+      );
+    },
+  },
+  {
+    Header: 'Reference',
+    accessor: row => {
+      return (
+        <Text fontSize={'16px'} fontWeight="400" color="#191919">
+          {row.reference}
+        </Text>
+      );
+    },
+  },
+  {
+    Header: 'Deposit',
+    accessor: row => {
+      return !row.deposit ? (
+        <Text fontSize="16px" fontWeight="500" color={themeStyles.color.matador__green}>
+          _
+        </Text>
+      ) : (
+        <FormatToColorfulAdaptiveCurrency
+          amount={row?.deposit}
+          lens={13}
+          color={themeStyles.color.matador__green}
+          maxSize={16}
+          minSize={10}
+          pow={0.92}
+          fontWeight="500"
+          decimalStyle={{fontWeight: '500', color: '#606060'}}
+        />
+      );
+    },
+  },
+
+  {
+    Header: 'Debit',
+    accessor: row => {
+      return !row.debit ? (
+        <Text fontSize="16px" fontWeight="500" color={'#f04438'}>
+          _
+        </Text>
+      ) : (
+        <FormatToColorfulAdaptiveCurrency
+          amount={row?.debit}
+          lens={13}
+          color={'#f04438'}
+          maxSize={16}
+          minSize={10}
+          pow={0.92}
+          fontWeight="500"
+          decimalStyle={{fontWeight: '500', color: '#606060'}}
+        />
+      );
+    },
+  },
+
+  {
+    Header: 'Net Transaction',
+
+    accessor: row => {
+      return !row.netTransaction ? (
+        <Text fontSize="16px" fontWeight="500" color={themeStyles.color.primary}>
+          _
+        </Text>
+      ) : (
+        <FormatToColorfulAdaptiveCurrency
+          amount={row?.netTransaction}
+          lens={13}
+          color={themeStyles.color.primary}
+          maxSize={16}
+          minSize={10}
+          pow={0.92}
+          fontWeight="500"
+          decimalStyle={{fontWeight: '500', color: '#606060'}}
+        />
+      );
+    },
+  },
+];
